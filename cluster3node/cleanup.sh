e338c90d7726bece494330a3cc129df7ef6a0b41
Jason Haugland <jasonhaugland@Jasons-MacBook-Pro.local>
2019-11-14 11:24:50 -0600

    change cleanup to use docker-compose

--- cluster3node/cleanup.sh
+++ cluster3node/cleanup.sh
@@ -3,7 +3,7 @@
 while true; do
     read -p "Do you wish to uninstall Redis Enterprise from your system?" yn
     case $yn in
-        [Yy]* ) sudo docker kill re-node1;sudo docker rm re-node1;sudo docker kill re-node2;sudo docker rm re-node2;sudo docker kill re-node3;sudo docker rm re-node3; break;;
+        [Yy]* ) docker-compose down; break;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
     esac
