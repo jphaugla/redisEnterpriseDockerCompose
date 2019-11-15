d1e0e1d49f7d552ff028e73f91ae54c232f4d7e3
Jason Haugland <jasonhaugland@Jasons-MacBook-Pro.local>
2019-11-15 12:20:29 -0600

    add cleanup and destroy scripts for crdb2node

---
+++ crdb2node/remove_volumes.sh
@@ -0,0 +1,4 @@
+echo "removing docker volumes"
+docker volume rm redis-enterprise-docker_redispers1
+docker volume rm redis-enterprise-docker_redispers2
+docker volume rm redis-enterprise-docker_redispers3
