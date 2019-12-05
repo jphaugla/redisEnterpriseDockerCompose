974a876a68c1e4a576f6379aec807f00c206e1e7
Jason Haugland <jasonhaugland@Jasons-MacBook-Pro-2.local>
2019-12-05 16:35:20 -0600

    dns fixed

--- dnscluster/bkup/redis-cli.sh
+++
@@ -1,1 +0,0 @@
-sudo docker run -v /opt/redislabs/resolv.conf:/etc/resolv.conf  --net redislabs  -it redis bash 
--- dnscluster/reset_north_cluster.sh
+++ dnscluster/reset_north_cluster.sh
@@ -7,13 +7,13 @@ docker volume rm dnscluster_redispers3
 docker volume rm dnscluster_redislogs1
 docker volume rm dnscluster_redislogs2
 docker volume rm dnscluster_redislogs3
-docker volume rm dnscluster_binddata
+# docker volume rm dnscluster_binddata
 
 echo "starting the containers"
 docker-compose up -d
 echo "running iptables on redis nodes"
-# sudo docker exec --user root n1 bash -c "iptables -t nat -I PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 5300"
-# sudo docker exec --user root n2 bash -c "iptables -t nat -I PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 5300"
-# sudo docker exec --user root n3 bash -c "iptables -t nat -I PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 5300"
+sudo docker exec --user root n1 bash -c "iptables -t nat -I PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 5300"
+sudo docker exec --user root n2 bash -c "iptables -t nat -I PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 5300"
+sudo docker exec --user root n3 bash -c "iptables -t nat -I PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 5300"
 echo "sleeping for 1 minute"
 sleep 60
