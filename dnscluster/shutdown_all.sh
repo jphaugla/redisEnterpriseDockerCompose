f1ce4e5c1dc10ae001346d7779db2dfee91fcd4d
Jason Haugland <jasonhaugland@Jasons-MacBook-Pro-2.local>
2019-12-14 08:51:33 -0600

    full shutdown

--- dnscluster/shutdown_all.sh
+++ dnscluster/shutdown_all.sh
@@ -1,5 +1,5 @@
 echo "shutting down the containers"
-docker-compose -f docker-compose-south.yml -f docker-compose.yml down
+docker-compose -f docker-compose-south.yml -f docker-compose.yml -f docker-compose-prom.yml -f docker-compose-bind.yml down
 echo "removing the north volumes"
 ./remove_north_volumes.sh
 echo "removing the south volumes"
