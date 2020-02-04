753003d3e9a6a564ded50796808e5022616024d8
Jason Haugland <jasonhaugland@Jasons-MacBook-Pro-2.local>
2020-02-04 15:10:27 -0600

    cleanup

--- crdb2node/testdecr.sh
+++ crdb2node/testdecr.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # test dercrement
 echo "testing decrement"
-sudo docker cp decrement.sh re-node2:/opt/decrement.sh
-sudo docker exec --user root -it re-node2 bash -c "chmod 777 /opt/decrement.sh"
-sudo docker exec --user root -it re-node2 bash -c "/opt/decrement.sh"
+docker cp decrement.sh re-node2:/opt/decrement.sh
+docker exec  -it re-node2 bash -c "/opt/decrement.sh"
--- crdb2node/testincr.sh
+++ crdb2node/testincr.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # kick off increment script
 echo "run increment"
-sudo docker cp increment.sh re-node1:/opt/increment.sh
-sudo docker exec --user root -it re-node1 bash -c "chmod 777 /opt/increment.sh"
-sudo docker exec --user root -it re-node1 bash -c "/opt/increment.sh"
+docker cp increment.sh re-node1:/opt/increment.sh
+docker exec -it re-node1 bash -c "/opt/increment.sh"
