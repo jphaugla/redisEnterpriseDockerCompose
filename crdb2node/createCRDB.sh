deb807bb12df79d316a252505eee2d24071ec3b5
Jason Haugland <jasonhaugland@Jasons-MacBook-Pro-2.local>
2020-02-04 13:47:38 -0600

    reasonable memory

--- crdb2node/createCRDB.sh
+++ crdb2node/createCRDB.sh
@@ -3,7 +3,7 @@
 echo "Creating demo-db database..."
 rm create_crdb.sh
 tee -a create_crdb.sh <<EOF
-curl -v -k -u REDemo@redislabs.com:redis123  -H "Content-Type: application/json" -X POST -d '{"default_db_config": {"name": "sample-crdb", "bigstore": false, "data_persistence": "aof", "replication": false, "memory_size": 1024000, "aof_policy": "appendfsync-every-sec", "snapshot_policy": [], "shards_count": 1, "port": 12005}, "instances": [{"cluster": {"url": "http://cluster1.local:8080", "credentials": {"username": "REDemo@redislabs.com", "password": "redis123"}, "name": "cluster1.local"}, "compression": 6}, {"cluster": {"url": "http://cluster2.local:8080", "credentials": {"username": "REDemo@redislabs.com", "password": "redis123"}, "name": "cluster2.local"}, "compression": 6}], "name": "sample-crdb"}' http://localhost:8080/v1/crdbs
+curl -v -k -u REDemo@redislabs.com:redis123  -H "Content-Type: application/json" -X POST -d '{"default_db_config": {"name": "sample-crdb", "bigstore": false, "data_persistence": "aof", "replication": false, "memory_size": 102400000, "aof_policy": "appendfsync-every-sec", "snapshot_policy": [], "shards_count": 1, "port": 12005}, "instances": [{"cluster": {"url": "http://cluster1.local:8080", "credentials": {"username": "REDemo@redislabs.com", "password": "redis123"}, "name": "cluster1.local"}, "compression": 6}, {"cluster": {"url": "http://cluster2.local:8080", "credentials": {"username": "REDemo@redislabs.com", "password": "redis123"}, "name": "cluster2.local"}, "compression": 6}], "name": "sample-crdb"}' http://localhost:8080/v1/crdbs
 EOF
 chmod 755 create_crdb.sh
 docker cp create_crdb.sh re-node1:/opt/create_crdb.sh
