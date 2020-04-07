eaeaba94b21517b2ec66da18f0489d6ee3245ce7
Jason Haugland <jasonhaugland@Jasons-MacBook-Pro-2.local>
2020-04-06 20:00:28 -0500

    fix ips on create_north

--- dnscluster/create_north_cluster.sh
+++ dnscluster/create_north_cluster.sh
@@ -1,9 +1,9 @@
-sudo docker exec -it n1 bash -c "/opt/redislabs/bin/rladmin cluster create persistent_path \
- /var/opt/redislabs/persist  ephemeral_path /var/opt/redislabs/tmp  addr  172.18.0.21  \
-name north.redislabs-training.org username admin@redislabs-training.org  password admin";
+# sudo docker exec -it n1 bash -c "/opt/redislabs/bin/rladmin cluster create persistent_path \
+#  /var/opt/redislabs/persist  ephemeral_path /var/opt/redislabs/tmp  addr  172.21.1.1  \
+# name north.redislabs-training.org username admin@redislabs-training.org  password admin";
 sudo docker exec n2 bash -c "/opt/redislabs/bin/rladmin cluster join persistent_path \
-/var/opt/redislabs/persist  ephemeral_path /var/opt/redislabs/tmp  addr 172.18.0.22   \
-username admin@redislabs-training.org  password admin nodes 172.18.0.21"
+/var/opt/redislabs/persist  ephemeral_path /var/opt/redislabs/tmp  addr 172.21.1.2   \
+username admin@redislabs-training.org  password admin nodes 172.21.1.1"
 sudo docker exec n3 bash -c "/opt/redislabs/bin/rladmin cluster join persistent_path \
-/var/opt/redislabs/persist  ephemeral_path /var/opt/redislabs/tmp  addr  172.18.0.23  \
-username admin@redislabs-training.org  password admin nodes 172.18.0.21"
+/var/opt/redislabs/persist  ephemeral_path /var/opt/redislabs/tmp  addr  172.21.1.3  \
+username admin@redislabs-training.org  password admin nodes 172.21.1.1"
--- dnscluster/reset_north_cluster.sh
+++ dnscluster/reset_north_cluster.sh
@@ -1,11 +1,13 @@
 echo "shutting down the containers"
-docker-compose -f docker-compose.yml -f docker-compose-bind.yml -f docker-compose-prom.yml down
+# docker-compose -f docker-compose.yml -f docker-compose-bind.yml -f docker-compose-prom.yml down
+docker-compose -f docker-compose.yml -f docker-compose-bind.yml -f docker-compose-jupyter.yml down
 echo "removing the volumes"
 ./remove_north_volumes.sh
 # docker volume rm dnscluster_binddata
 
 echo "starting the containers"
-docker-compose -f docker-compose.yml -f docker-compose-prom.yml -f docker-compose-bind.yml up -d
+docker-compose -f docker-compose.yml -f docker-compose-jupyter.yml -f docker-compose-bind.yml up -d
+# docker-compose -f docker-compose.yml -f docker-compose-prom.yml -f docker-compose-bind.yml up -d
 echo "running iptables on redis nodes"
 ./north_ip_tables.sh
 echo "sleeping for 1 minute"
